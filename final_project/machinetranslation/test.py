ad6f6c68339376b15ff0c103d7d9a002c3621fd7
DonAbram <123044695+DonAbram@users.noreply.github.com>
2023-01-23 14:38:58 +0000

    Stuck at translator error

---
+++ final_project/machinetranslation/test.py
@@ -0,0 +1,26 @@
+import unittest
+
+from translator import englishToFrench, frenchToEnglish
+
+class test_translator (unittest.TestCase):
+
+    def test_englishToFrench(self):
+
+        self.assertEqual(englishToFrench(None), None)
+
+        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+
+    def test_frenchToEnglish(self):
+
+        self.assertEqual(frenchToEnglish(None), None)
+
+        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
+
+if __name__=='_main_':
+    unittest.main()
+
+#Inorder to avoid running this program on CLI to get any response
+#the following code can be used
+
+suite = unittest.TestLoader().loadTestsFromTestCase(test_translator)
+unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,40 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+apikey = os.environ['apikey']
+
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+#translation = language_translator.translate(
+#    text='Hello, how are you today?',
+#    model_id='en-es').get_result()
+#print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+def englishToFrench(englishText):
+    if englishText is None:
+        return None
+ 
+    else:
+        frenchText = language_translator.translate(englishText, model_id = 'en-fr').get_result()
+        return frenchText
+
+def frenchToEnglish(frenchText):
+
+    if frenchText is None:
+        return None
+
+    else:
+        englishText = language_translator.translate(frenchText, model_id = 'en-fr').get_result()
+        return englishText
