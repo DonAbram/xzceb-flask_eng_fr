4983e0309670467d42cdc38420ca2e9c89f91a0c
DonAbram <123044695+DonAbram@users.noreply.github.com>
2023-01-24 03:28:26 +0000

    Pylint passed new folder structure

---
+++ final_project/machinetranslation/tests/test.py
@@ -0,0 +1,26 @@
+import unittest
+
+from translator import english_to_french, french_to_english
+
+class test_translator (unittest.TestCase):
+
+    def test_english_to_french(self):
+
+        self.assertEqual(english_to_french(None), None)
+
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+    def test_french_to_english(self):
+
+        self.assertEqual(french_to_english(None), None)
+
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+if __name__=='_main_':
+    unittest.main()
+
+#Inorder to avoid running this program on CLI to get any response
+#the following code can be used
+
+suite = unittest.TestLoader().loadTestsFromTestCase(test_translator)
+unittest.TextTestRunner(verbosity=2).run(suite)
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -24,28 +24,28 @@ language_translator.set_service_url(url)
 
 
 
-def englishToFrench(englishText):
-    if englishText is None:
+def english_to_french(english_text):
+    if english_text is None:
         return None
  
     else:
-        frenchText1 = language_translator.translate(englishText, model_id = 'en-fr').get_result()
-        frenchText2 = frenchText1['translations']
-        frenchText = frenchText2[0]['translation']
+        french_text1 = language_translator.translate(english_text, model_id = 'en-fr').get_result()
+        french_text2 = french_text1['translations']
+        french_text = french_text2[0]['translation']
 
-        return frenchText
+        return french_text
 
-def frenchToEnglish(frenchText):
+def french_to_english(french_text):
 
-    if frenchText is None:
+    if french_text is None:
         return None
 
     else:
-        englishText1 = language_translator.translate(frenchText, model_id = 'fr-en').get_result()
-        englishText2 = englishText1['translations']
-        englishText = englishText2[0]['translation']
-        return englishText
+        english_text1 = language_translator.translate(french_text, model_id = 'fr-en').get_result()
+        english_text2 = english_text1['translations']
+        english_text = english_text2[0]['translation']
+        return english_text
 
 #test
-#print(json.dumps(englishToFrench('Hello'), indent=2, ensure_ascii=False))
-#print(json.dumps(frenchToEnglish('Bonjour'), indent=2, ensure_ascii=False))
+#print(json.dumps(english_to_french('Hello'), indent=2, ensure_ascii=False))
+#print(json.dumps(french_to_english('Bonjour'), indent=2, ensure_ascii=False))
