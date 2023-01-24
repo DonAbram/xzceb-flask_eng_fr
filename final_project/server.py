ec53d3827b8b9b8a345b34ab6798b2baf79aec58
DonAbram <123044695+DonAbram@users.noreply.github.com>
2023-01-24 04:01:58 +0000

    Stuck at index.html error

--- final_project/server.py
+++ final_project/server.py
@@ -7,18 +7,18 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
+    print(json.dumps(english_to_french(textToTranslate), indent=2, ensure_ascii=False))
     return "Translated text to French"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
+    print(json.dumps(french_to_english(textToTranslate), indent=2, ensure_ascii=False))
     return "Translated text to English"
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
